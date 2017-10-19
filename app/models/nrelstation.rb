bafec5b307d04739541d33e0ebde01e4450393ca
snayrouz <samuel.nayrouz@gmail.com>
2017-10-19 12:42:17 -0600

    Add initialize to station poro and private attrs

--- app/models/nrelstation.rb
+++ app/models/nrelstation.rb
@@ -1,5 +1,12 @@
 class NrelStation
-
+  def initialize(attrs = {})
+    @attrs = attrs
+    @name = attrs[:station_name]
+    @address = attrs[:street_address]
+    @fuel_type = attrs[:fuel_type_code]
+    @distance = attrs[:distance]
+    @access_times = attrs[:access_days_time]
+  end
 
 
   private
